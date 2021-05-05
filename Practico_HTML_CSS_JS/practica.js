db1ad71e5e7c76f2a0f2112287c427fcf285805a
Ezequiel Alvarellos <ezealvarellos@gmail.com>
2021-05-04 23:41:50 -0300

    Practica mejorada

--- Practico_HTML_CSS_JS/practica.js
+++ Practico_HTML_CSS_JS/practica.js
@@ -1,30 +1,37 @@
-function validarNombre() {
-    var nombre;
-    var apellido;
-  
-    // Get the value of the input field with id="numb"
-    nombre = document.getElementById("nombre").value;
-    apellido = document.getElementById("apellido").value;
-    form = document.getElementById("formulario");
-  
-    // If x is Not a Number or less than one or greater than 10
-    if((apellido == null || apellido == '') && (nombre == null || nombre == '')) {
-        alert("Ingrese un nombre y apellido"); 
-        form.reset();
-    }
-    else if (nombre == null || nombre == '') {
-        alert("Ingrese un nombre");   
-        form.reset(); 
-    }
-    else if(apellido == null || apellido == '') {
-        alert("Ingrese un apellido");
-        form.reset();   
-    }
+document.addEventListener("DOMContentLoaded", () => {
 
+    function validacionDeCampos() {
+        const formulario = document.querySelector('.formulario');
 
+        formulario.addEventListener('submit', (event) => {
+            const validarCampos = document.querySelectorAll('.validacionFormulario');
+            let estaVacio = false;
     
-}
+            validarCampos.forEach(campo => {
+                if(campo.value === '') {
+                    estaVacio = true;
+                    alert(`El campo ${campo.name} esta vacío`);
+                }
+            });
+    
+            if(estaVacio) {
+                event.preventDefault();
+            }
+        });
+    }
 
-function limpiarFormulario() {
-    document.getElementById(form).reset();
-  }
+    function limpiarFormulario() {
+        const botonReset = document.querySelector('.botonReset');
+        
+        botonReset.addEventListener('click', () => {
+            const campos = document.querySelectorAll('input');
+
+            for(let i=0; i < campos.length; i++) {
+                campos[i].value = '';
+            }
+        });
+    }
+
+    validacionDeCampos();
+    limpiarFormulario();
+});
