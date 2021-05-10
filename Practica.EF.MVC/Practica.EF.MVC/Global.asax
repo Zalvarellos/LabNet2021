51e23c10ee1f4713696bafee6defbdc8dc36eeef
Ezequiel Alvarellos <ezealvarellos@gmail.com>
2021-05-09 23:58:08 -0300

    Me falta el update

---
+++ Practica.EF.MVC/Practica.EF.MVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Practica.EF.MVC.MvcApplication" Language="C#" %>
---
+++ Practica.EF.MVC/Practica.EF.MVC/Models/CustomerView.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Practica.EF.MVC.Models
+{
+    public class CustomerView
+    {
+        public string Id { get; set; }
+        public string Nombre { get; set; }
+        public string Empresa { get; set; }
+        public string Contacto { get; set; }
+    }
+}
